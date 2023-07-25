8f4696a5e676b6df1d2f4889da0015ef2934419f
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-07-25 10:09:43 +0200

    Fix possible null references in modal interaction scripts
    Added null checks for dialog and closeButton in script.js and script.min.js. These changes ensure that an event listener does not get added to an undefined object, which could cause an error. This could happen if modal-dialog or close-btn elements are not available, which is a scenario that these scripts did not previously account for. This fix makes the code more robust against changes on the HTML side.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const dialog = document.getElementById('modal-dialog');
-  const closeButton = document.getElementById('close-btn');
+	const dialog = document.getElementById('modal-dialog');
+	const closeButton = document.getElementById('close-btn');
 
-  // Close dialog
-  closeButton.addEventListener('click', function() {
-    closeDialog();
-  });
+	if (dialog && closeButton) {
 
-  // Close dialog on ESC
-  window.addEventListener('keydown', function(event) {
-    if (event.key === 'Escape' && dialog.open) {
-      closeDialog();
-    }
-  });
+		// Close dialog
+		closeButton.addEventListener('click', function() {
+			closeDialog();
+		});
 
-  // Reappear only the next day
-  const now = new Date();
-  const today = now.toDateString(); // convert the date to a string
-  const lastShownDate = localStorage.getItem('modalLastShownDate');
+		// Close dialog on ESC
+		window.addEventListener('keydown', function(event) {
+			if (event.key === 'Escape' && dialog && dialog.open) {
+				closeDialog();
+			}
+		});
+	}
 
-  if (lastShownDate === null || lastShownDate !== today) {
-    localStorage.setItem('modalLastShownDate', today);
-    dialog.showModal();
-    document.body.classList.add('modal-open');
-  }
+	// Reappear only the next day
+	const now = new Date();
+	const today = now.toDateString(); // convert the date to a string
+	const lastShownDate = localStorage.getItem('modalLastShownDate');
 
-  function closeDialog() {
-    dialog.close();
-    document.body.classList.remove('modal-open');
-  }
+	if (lastShownDate === null || lastShownDate !== today) {
+		localStorage.setItem('modalLastShownDate', today);
+		dialog.showModal();
+		document.body.classList.add('modal-open');
+	}
+
+	function closeDialog() {
+		dialog.close();
+		document.body.classList.remove('modal-open');
+	}
 })
